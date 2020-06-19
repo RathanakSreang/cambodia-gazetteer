8c2c4c7079a59b3cd70d374c82a6399695bb69ba
Sreang Rathanak <sreangrathanak@yahoo.com>
2020-06-19 11:05:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var cambodia_gazetterr = require('./cambodia_gazetteer.json');
+
+module.exports = {
+  cambodia_gazetterr: cambodia_gazetterr
+}
---
+++ index.test.js
@@ -0,0 +1,29 @@
+var {cambodia_gazetterr} = require('./index');
+var banteayMeanchey = cambodia_gazetterr[0];
+
+test('Provinces', () => {
+  expect(banteayMeanchey["khmer"]).toBe("បន្ទាយមានជ័យ");
+  expect(banteayMeanchey["latin"]).toBe("Banteay Meanchey");
+})
+
+var districts = banteayMeanchey.districts;
+test('Districts', () => {
+  expect(districts.length).toBe(9);
+  expect(districts[0]["khmer"]).toBe("មង្គលបូរី");
+  expect(districts[0]["latin"]).toBe("Mongkol Borei");
+})
+
+var communes = districts[0].communes;
+
+test('communes', () => {
+  expect(communes.length).toBe(13);
+  expect(communes[0]["khmer"]).toBe("បន្ទាយនាង");
+  expect(communes[0]["latin"]).toBe("Banteay Neang");
+})
+
+var villages = communes[0].villages;
+test('villages', () => {
+  expect(villages.length).toBe(19);
+  expect(villages[0]["khmer"]).toBe("អូរធំ");
+  expect(villages[0]["latin"]).toBe("Ou Thum");
+})
