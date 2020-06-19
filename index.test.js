a11be60ffb51736ac84c94aa76e62b6f8b8fbe03
Sreang Rathanak <sreangrathanak@yahoo.com>
2020-06-19 11:31:32 +0700

    first commit

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ var banteayMeanchey = cambodia_gazetterr[0];
 
 test('Provinces', () => {
   expect(banteayMeanchey["khmer"]).toBe("បន្ទាយមានជ័យ");
-  expect(banteayMeanchey["latin"]).toBe("Banteay Meanchey");
+  expect(banteayMeanchey["latin"]).toBe("Banteay Meanchey1");
 })
 
 var districts = banteayMeanchey.districts;
